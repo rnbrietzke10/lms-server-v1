51c36900de74ffe4b94e41b9f6db3199b8d1e1c4
Rebecca Brietzke <rnbrietzke@outlook.com>
2024-03-06 08:49:39 -0600

    add routes for creating, updating and getting staff member with id

--- app.js
+++ app.js
@@ -3,10 +3,73 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const staff = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/staff.json`));
 
 app.get('/api/v1/staff', (req, res) => {
-  res.status(200).json({ status: 'success', data: { staff: staff } });
+  res
+    .status(200)
+    .json({ status: 'success', results: staff.length, data: { staff } });
+});
+
+// GET staff member by id
+app.get('/api/v1/staff/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const staffMember = staff.find((el) => el.id === id);
+
+  if (!staffMember) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      staffMember,
+    },
+  });
+});
+
+// create staff memeber
+app.post('/api/v1/staff', (req, res) => {
+  const newId = staff[staff.length - 1].id + 1;
+
+  const newStaff = Object.assign({ id: newId }, req.body);
+  staff.push(newStaff);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/staff.json`,
+    JSON.stringify(staff),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          staff: newStaff,
+        },
+      });
+    }
+  );
+});
+
+// Update post
+app.patch('/api/v1/staff/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const staffMember = staff.find((el) => el.id === id);
+
+  if (!staffMember) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  // TODO: implement when db is set up
+  res.status(200).json({
+    status: 'success',
+    data: 'updated staff member object',
+  });
 });
 
 const port = 3000;
