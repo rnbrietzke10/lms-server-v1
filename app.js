8315a5d2875cb89d36376b4e182d259ce9c969dc
Rebecca Brietzke <rnbrietzke@outlook.com>
2024-03-12 09:57:26 -0500

    create server.js file to run server, add script to start app in package.json

--- app.js
+++ app.js
@@ -29,8 +29,5 @@ app.use('/api/v1/staff', staffRouter);
 app.use('/api/v1/courses', courseRouter);
 
 // Start Server
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
